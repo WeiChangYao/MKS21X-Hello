5c26c9e26d272de01e9c4ff903573dbde881a817
weichanhyao <wyao1@stuy.edu>
2018-09-16 21:51:36 -0400

    code that works

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+   public static void main(String[] args) {
+      // Prints "Hello my name is Wei Chang Yao, waddup" in the terminal window.
+      System.out.println("Hello my name is Wei Chang Yao, waddup");
+   }
+}
